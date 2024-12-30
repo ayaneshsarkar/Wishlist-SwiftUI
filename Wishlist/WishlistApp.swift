dcdb020f175acc2edfbb2268177a7782fda3557a
Symlinktech LLP <symlinktech@gmail.com>
2024-12-30 20:56:08 +0530

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Ayanesh Sarkar on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
