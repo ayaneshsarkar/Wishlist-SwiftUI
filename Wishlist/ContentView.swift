23a13027c48007d865b351b64f6e2694b52ff3f5
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-12-30 21:42:31 +0530

    Implemented the project with swift data

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftData
 struct ContentView: View {
   @Environment(\.modelContext) private var modelContext
   @Query private var wishes: [Wish]
+  @State private var isAlertShowing: Bool = false
+  @State private var title: String = ""
   
   var body: some View {
     NavigationStack {
@@ -19,9 +21,40 @@ struct ContentView: View {
           Text(wish.title)
             .font(.title.weight(.light))
             .padding(.vertical, 2)
+            .swipeActions {
+              Button("Delete", role: .destructive) {
+                modelContext.delete(wish)
+              }
+            }
         }
       } // List
       .navigationTitle("Wishlist")
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button {
+            isAlertShowing.toggle()
+          } label: {
+            Image(systemName: "plus")
+              .imageScale(.large)
+          }
+        }
+        
+        if wishes.isEmpty != true {
+          ToolbarItem(placement: .bottomBar) {
+            Text("\(wishes.count) wish\(wishes.count > 1 ? "es" : "")")
+          }
+        }
+      }
+      .alert("Create a new wish", isPresented: $isAlertShowing) {
+        TextField("Enter a wish", text: $title)
+        
+        Button {
+          modelContext.insert(Wish(title: title))
+          title = ""
+        } label: {
+          Text("Save")
+        }
+      }
       .overlay {
         if wishes.isEmpty {
           ContentUnavailableView(
